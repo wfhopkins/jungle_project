c68eca8684a8b79f5ba50487577460b4c3481f72
wfhopkins <will.hop@gmail.com>
2023-08-01 23:36:21 -0700

    add cypress test for home page load

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,14 @@
+describe('Jungle App Home Page', () => {
+  beforeEach(() => {
+
+    cy.visit('/')
+  })
+
+  it("There are products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There are 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+})
